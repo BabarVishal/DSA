b01e4ecc7164c71b4e93e9940202bc1211426f7b
vishal <vishalbabar7351@gmail.com>
2024-08-20 15:21:05 +0530

    Conditinal Oprater

--- day2_DSA/index.cpp
+++ day2_DSA/index.cpp
@@ -14,6 +14,22 @@ bool myBoll = true;
 
 double mydouble = 22.33;
 
-cout << "Hy I Am Back With DSA!";
+cout << "Hy I Am Back With DSA!\n";
 
+int A = 22;
+int B = 33;
+
+int fisth = (A == B);
+cout << fisth << endl;
+
+int second = (A != B);
+cout << second << endl;
+
+int Three = (A > B);
+cout << Three << endl;
+
+int four = (A < B);
+cout << four << endl;
+
+return 0;
 }
