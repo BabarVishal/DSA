984ba6fb5aa9d652e96fec47dc5ceef31bba80e1
vishal <vishalbabar7351@gmail.com>
2024-08-20 15:09:40 +0530

    first commit

---
+++ day2_DSA/index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+int a = 22;
+
+string myLetter = "Vishal Babar";
+
+char myChar = 's';
+
+bool myBoll = true;
+
+double mydouble = 22.33;
+
+cout << "Hy I Am Back With DSA!";
+
+}
